deea44354bd94e17bff12603d112988745749096
Michel Fernandes Góes <michelfgoes1998@gmail.com>
2025-06-07 21:42:04 -0300

    Implementação do Describe nos testes de soma

--- some.test.js
+++ some.test.js
@@ -1,5 +1,9 @@
 const library_module = require('./some')
 
-test("Deve verificar o resultado de uma soma", () => {
-    expect(library_module(3, 8)).toBe(11)
+describe("Testes de soma", () => {
+    test("Deve verificar o resultado correto de uma soma", () => {
+        expect(library_module(3, 8)).toBe(11)})
+
+    test("Deve verificar o resultado incorreto de uma soma", () => {
+        expect(library_module(3, 8)).not.toBe(12)})
 })
